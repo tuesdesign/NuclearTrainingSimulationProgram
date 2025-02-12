93377de78fd9cb702dda92f2281316eb57bd8df8
ChiaLoChen <94023698+ChiaLoChen@users.noreply.github.com>
2025-02-12 18:00:09 -0500

    basic button settings

---
+++ Assets/Chia Lo's code/SweepButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweepButton : MonoBehaviour
+{
+    private Task_List task;
+    // Start is called before the first frame update
+    void Start()
+    {
+        task = FindObjectOfType<Task_List>();
+    }
+
+    public void StadiumSweepStart()
+    {
+        task.changeMode(0);
+    }
+
+    public void ParkingLotSweepStart()
+    {
+        task.changeMode(1);
+    }
+}
--- Assets/Chia Lo's code/Task_List.cs
+++ Assets/Chia Lo's code/Task_List.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Task_List : MonoBehaviour
 {
+    private int sweepMode = 0;
+
+    public TextMeshProUGUI mode;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mode.text = "Sweep Mode";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (sweepMode)
+        {
+            case 0 :
+                mode.text = "stadium sweep start";
+                break;
+            case 1 :
+                mode.text = "parking lot sweep start";
+                break;
+        }
+    }
+
+    public void changeMode(int mode)
+    {
+        sweepMode = mode;
     }
 }
